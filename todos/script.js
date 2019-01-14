925d7186de040be03557322ee6223c0eb9bbad21
t-ikuta <happy0learner@gmail.com>
2019-01-14 22:06:26 +0900

    Finish basic features

--- todos/script.js
+++ todos/script.js
@@ -12,10 +12,9 @@ const App = {
     const top = $('html').scrollTop();
     $('.delete-modal').offset({ top: top + 200 });
   },
-  deleteTodo() {
-    const self = this;
+  deleteTodo(id) {
     $('li').filter(function() {
-      return $(this).attr('data-id') === self.id;
+      return $(this).attr('data-id') === id;
     }).remove();
 
     // const lis = Array.prototype.slice.call(document.querySelectorAll('li')).filter(el => {
@@ -25,20 +24,50 @@ const App = {
 
     this.deleteOvelayAndModal();
   },
-  handleTodoDeletion() {
-    $('.overlay, .delete-no').on('click', e => this.deleteOvelayAndModal());
-    $('.delete-yes').on('click', e => this.deleteTodo())
-  },
-  handleTodoModal() {
-    $('.delete').on('click', e => {
+  handleTodoDeletion(id) {
+    $(document).on('click', e => {
       e.preventDefault();
-      this.id = $(e.target).parent('li').attr('data-id');
-      this.showModal();
-      this.handleTodoDeletion();
+
+      var $e = $(e.target);
+      if ($e.prop('tagName') === 'LI' && $e.attr('class') === 'delete') {
+        this.deleteTodo(id);
+      }
+
+      $('.contextmenu').remove();
     })
+  },
+  // handleTodoDeletion() {
+  //   $('.overlay, .delete-no').on('click', e => this.deleteOvelayAndModal());
+  //   $('.delete-yes').on('click', e => this.deleteTodo())
+  // },
+  handleTodoModal() {
+    // $('li').on('contextmenu', e => {
+    //   e.preventDefault();
+    //   // this.id = $(e.target).parent('li').attr('data-id');
+    //   // this.showModal();
+    //   // this.handleTodoDeletion();
+    // })
+  },
+  handleContextMenu() {
+    $('li').on('contextmenu', e => {
+      e.preventDefault();
+      const id = $(e.target).attr('data-id');
+      this.renderContextMenu(id, e.pageX, e.pageY);
+    });
   },
   deleteOvelayAndModal() {
     $('.overlay, .delete-modal').hide();
+  },
+  renderContextMenu(id, x, y) {
+    // remove the previously shown context menu
+    $('.contextmenu').remove();
+
+    // render the current context menu
+    const templateHTML = $('#contextmenu-template').html();
+    const contextmenuTemplate = Handlebars.compile(templateHTML);
+    const contextmenuHTML = contextmenuTemplate({id: id});
+    $(contextmenuHTML).insertAfter('h1').offset({top: y, left: x}).fadeIn(200);
+    this.handleTodoDeletion(id);
   },
   renderTodos() {
     const templateHTML = $('#todos-template').html();
@@ -48,8 +77,9 @@ const App = {
   },
   init() {
     this.renderTodos();
-    this.handleTodoModal();
-    this.id = null;
+    this.handleContextMenu();
+    // this.handleTodoModal();
+    // this.id = null;
   }
 }
 
